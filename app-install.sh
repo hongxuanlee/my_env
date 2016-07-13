49812642481a5cc5a0927a9625b95cfe45a8b2de
宣予 <hongxuan.lhx@alibaba-inc.com>
2016-07-14 00:50:27 +0800

    app-install

--- app-install.sh
+++ app-install.sh
@@ -11,20 +11,38 @@ function install_app(){
     filename="$name$type"
     echo "\033[32m install $name \033[0m\n"
     echo $name $address $filename $t 
+    if [ -d "/Applications/$name.app" ]; then
+        echo "$name installed"
+        return
+    fi
+    cd $DEPS_DIR
+    [ ! -e $DEPS_DIR/$filename ] && curl -o $filename $address  
     if [ "$type" -eq "dmg" ]; then
         cd $DEPS_DIR
-        [ ! -e $DEPS_DIR/$filename ] && curl -o $filename $address  
         hdiutil attach "$filename"
-        cd /Volumes/Shadowsocks
-        sudo cp -R "$name.app" ./Applications
+        cd "/Volumes/$filename"
+        sudo mv -R "$name.app" ./Applications
         cd $DEPS_DIR
         hdiutil detach "/Volumes/$name"   
     fi
+    if [ "$type" -eq "zip" ]; then
+        cd $DEPS_DIR
+        unzip filename
+        hdiutil attach "$filename"
+        cd "/Volumes/$filename"
+        sudo mv -R "$name.app" ./Applications
+        cd $DEPS_DIR
+        hdiutil detach "/Volumes/$name"  
+    fi   
 }
 
 while read LINE
 do
-    install_app $LINE
+    name=`echo $LINE | awk -F ',' '{print $1}'`
+    t=`echo $LINE | awk -F ',' '{print $2}'`
+    address=`echo $LINE | awk -F ',' '{print $3}'`
+    echo $name
+    install_app "$name" "$t" "$address"
 done < "$CUR_DIR/app.conf"
 
 
